f5f176de7dc4431b6395c0ba0d17f5116d160c11
martos93 <jesusma93@gmail.com>
2017-04-15 10:37:34 +0200

    Arreglado test CU3

--- src/test/java/usecases/CU3.java
+++ src/test/java/usecases/CU3.java
@@ -1,6 +1,67 @@
+
 package usecases;
 
+import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
 
-public class CU3 {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import domain.Chorbi;
+import repositories.ChorbiRepository;
+import services.ActorService;
+import services.ChorbiService;
+import utilities.AbstractTest;
+
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class CU3 extends AbstractTest {
+
+	@Autowired
+	private ChorbiService		chorbiService;
+	@Autowired
+	private ActorService		actorService;
+
+	@Autowired
+	private ChorbiRepository	chorbiRepository;
+
+
+	@Test
+	public void editChorbi() {
+		final Object[][] testingData = {
+			{
+				"chorbi1", null, "4556327836268273"
+			}, {
+				"chorbi1", ConstraintViolationException.class, "aaaaa"
+			},
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.template((String) testingData[i][0], (Class<?>) testingData[i][1], (String) testingData[i][2]);
+	}
+
+	protected void template(final String username, final Class<?> expected, final String arg) {
+		Class<?> caught;
+		caught = null;
+		try {
+			this.authenticate(username);
+			final Chorbi chorbi = this.chorbiService.getLoggedChorbi();
+			if (arg == "4556327836268273")
+				chorbi.getCreditCard().setNumber(arg);
+			else
+				chorbi.setPicture(arg);
+			this.chorbiService.save(chorbi);
+			this.chorbiRepository.flush();
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+		this.checkExceptions(expected, caught);
+	}
 }
